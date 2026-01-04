2d295c8c58f5417e522e6edd33f64cdab61883eb
Sergiy Sanin <sanin_sergiy@yahoo.com>
2026-01-04 18:14:36 +0000

    checkpoint

--- programs/solana-crowdfunding/src/lib.rs
+++ programs/solana-crowdfunding/src/lib.rs
@@ -1,13 +1,4 @@
-pub mod constants;
-pub mod error;
-pub mod instructions;
-pub mod state;
-
 use anchor_lang::prelude::*;
-
-pub use constants::*;
-pub use instructions::*;
-pub use state::*;
 
 declare_id!("2K1ddAKsCZKq9eXqL6xu64wtBJPCa4bzRQr547aw5wCo");
 
@@ -15,7 +6,44 @@ declare_id!("2K1ddAKsCZKq9eXqL6xu64wtBJPCa4bzRQr547aw5wCo");
 pub mod solana_crowdfunding {
     use super::*;
 
-    pub fn create(ctx: Context<Create>) -> Result<()> {
-        initialize::handler(ctx)
+    pub fn create(ctx: Context<Create>, name: String, description: String) -> Result<()> {
+        let campaign = &mut ctx.accounts.campaign;
+        campaign.name = name;
+        campaign.description = description;
+        campaign.amount_donated = 0;
+        campaign.admin = *ctx.accounts.user.key;
+        Ok(())
     }
+
+    pub fn create(ctx: Context<Create>, amount: uint64) -> Result<()> {
+        let campaign = &mut ctx.accounts.campaign;
+        let user = &mut ctx.accounts.user;
+        if admin.account != *user.key {
+            return Err(ProgramError::IncorrectProgramId);
+        }
+        let rent_balance = Rent::get()?.minimum_balance(campaign.to_account_info().data_len());
+        if campaign.to_account_info().lamports_borrow - rent_balance < amount {
+            return Err(ProgramError::InsufficientFunds);
+        }
+        **campaign.to_account_info().try_borrow_mut_lamports()? -= amount;
+        **user.to_account_info().try_borrow_mut_lamports()? += amount;
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Create<'info> {
+    #[account(init, payer=user, space=900, seeds=[b"CAMPAIGN_DEMO".as_ref(), user.key().as_ref()], bump)]
+    pub campaign: Account<'info, Calculator>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>
+}
+
+#[account]
+pub struct Campaign {
+    admin: Pubkey,
+    name: String,
+    description: String,
+    amount_donated: u64
 }
